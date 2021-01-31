8c210494eb8f87d9f86ddfb724af337e0b44c81a
SudharshanBhimisetty <sudharshanbhimisetty24@gmail.com>
2021-01-31 14:58:41 +0530

    initial commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Loading from './Loading'
+import Cocktail from './Cocktail'
+
+import { useGlobalContext } from '../context'
+
+
+
+const CocktailList = () => {
+
+    const {cocktails,loading} = useGlobalContext();
+    
+    if(loading){
+        return <Loading />
+    }
+    if(cocktails.length < 1){
+        return (
+        <h2 className="section-title">
+            No cocktails matched ur search
+        </h2>
+        )
+    }
+
+    return (
+        <section className="section">
+            <h2 className="section-title">
+        cocktails
+            </h2>
+            <div className="cocktails-center">
+                {cocktails.map((item) =>{
+                    return <Cocktail key={item.id} {...item}/>
+                })}
+            </div>
+        </section>
+    )
+    }
+
+export default CocktailList
